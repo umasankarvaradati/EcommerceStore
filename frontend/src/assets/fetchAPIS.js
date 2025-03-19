4cb196c807495be1fc6540ccce6ac00f59d82a66
Venu varma <konduruvenuvarma123@outlook.com>
2025-03-19 17:36:01 +0530

    integrated api

--- frontend/src/assets/fetchAPIS.js
+++ frontend/src/assets/fetchAPIS.js
@@ -1,7 +1,8 @@
-export const BANNER_API = "http://127.0.0.1:8000/api/homeimages/?category=banner";
+const API="https://ecommercestore-v655.onrender.com/api/"
+export const BANNER_API = API+"homeimages/?category=banner";
 export const BROWSE_BY_CATEGORY="http://127.0.0.1:8000/media/products/";
-export const LOGIN_API="http://127.0.0.1:8000/api/login/";
-export const REGISTER_API="http://127.0.0.1:8000/api/auth/";
-export const CART_API_URL='http://127.0.0.1:8000/api/cart/';
-export const CART_GET_URL="http://127.0.0.1:8000/api/getcart/";
-export const PRODUCT_API_URL='http://127.0.0.1:8000/api/products/';
+export const LOGIN_API=API+"api/login/";
+export const REGISTER_API=API+"auth/";
+export const CART_API_URL=API+"cart/";
+export const CART_GET_URL=API+"getcart/";
+export const PRODUCT_API_URL=API+"products/";
