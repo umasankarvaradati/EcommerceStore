ca6d7e55186632e4106fb0b6666e2079f3cdcebd
Venu varma <konduruvenuvarma123@outlook.com>
2025-03-19 18:11:12 +0530

    Fixed login api error

--- frontend/src/assets/fetchAPIS.js
+++ frontend/src/assets/fetchAPIS.js
@@ -1,7 +1,7 @@
 const API="https://ecommercestore-v655.onrender.com/api/"
 export const BANNER_API = API+"homeimages/?category=banner";
 export const BROWSE_BY_CATEGORY="https://ecommercestore-v655.onrender.com/media/products/";
-export const LOGIN_API=API+"api/login/";
+export const LOGIN_API=API+"login/";
 export const REGISTER_API=API+"auth/";
 export const CART_API_URL=API+"cart/";
 export const CART_GET_URL=API+"getcart/";
